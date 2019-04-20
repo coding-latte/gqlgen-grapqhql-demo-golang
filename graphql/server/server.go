2119355cce35ec4071568713e0ed3cb46c384aaf
Maina Wycliffe <wmmaina7@gmail.com>
2019-04-20 12:42:33 +0300

    added twitter client, to connect to twitter api
    passed the client to root resolver

--- graphql/server/server.go
+++ graphql/server/server.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"github.com/coding-latte/gqlgen-grapgql-demo/graphql"
+	"github.com/dghubble/go-twitter/twitter"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/handler"
-	"github.com/coding-latte/gqlgen-grapqhql-demo-golang"
+	"github.com/dghubble/oauth1"
 )
 
 const defaultPort = "8080"
+
+func graphqlHandler() http.Handler {
+
+	consumerKey := os.Getenv("CONSUMER_KEY")
+	consumerSecret := os.Getenv("CONSUMER_SECRET")
+
+	accessToken := os.Getenv("ACCESS_TOKEN")
+	accessSecret := os.Getenv("ACCESS_SECRET")
+
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	return handler.GraphQL(
+		graphql.NewExecutableSchema(
+			graphql.NewRootResolver(client),
+		),
+	)
+}
+
+// Defining the Playground handler
+func playgroundHandler() http.Handler {
+	return handler.Playground("GraphQL playground", "/query")
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,8 +47,8 @@ func main() {
 		port = defaultPort
 	}
 
-	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(gqlgen_grapqhql_demo_golang.NewExecutableSchema(gqlgen_grapqhql_demo_golang.Config{Resolvers: &gqlgen_grapqhql_demo_golang.Resolver{}})))
+	http.Handle("/", playgroundHandler())
+	http.Handle("/query", graphqlHandler())
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
